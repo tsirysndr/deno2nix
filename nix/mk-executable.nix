8dc430303cc0beca262db3107bb770a413ef2390
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-10-05 18:16:56 +0300

    Update mk-executable.nix

--- nix/mk-executable.nix
+++ nix/mk-executable.nix
@@ -32,6 +32,7 @@
   compileCmd = concatStringsSep " " (
     [
       "deno compile --cached-only"
+      "--allow-env"
       "--lock=${lockfile}"
       "--output=${bin}"
       # "--config=${config}"
